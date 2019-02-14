a827941f88d7f0138dd7574d85b4bf487e0d4d24
joetechem <joe@techemstudios.com>
2019-02-14 14:08:41 -0500

    indent

--- tutorials/a_basic/e.py
+++ tutorials/a_basic/e.py
@@ -22,7 +22,7 @@ class StringGenerator(object):
     def index(self):
         return """<html>
           <head>
-          <link href="/static/css/style.css" rel="stylesheet">
+            <link href="/static/css/style.css" rel="stylesheet">
           </head>
           <body>
             <form method="get" action="generate">
