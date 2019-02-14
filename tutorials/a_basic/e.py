6150cbd2bdc9f5025fbfa357c5959e083eedca28
joetechem <joe@techemstudios.com>
2019-02-14 13:30:01 -0500

    functionality/UI/static resources

--- tutorials/a_basic/e.py
+++ tutorials/a_basic/e.py
@@ -1,8 +1,13 @@
 """
-Track User Activity
+Track User Activity (commit 34209f87b70ecfa8d11040cb5bbd54b19fafea48)
 - go to http://localhost:8080/
 - input a number, this will generate a random string (as before)
 - http://localhost:8080/display to see the string
+
+Functionality & UI
+- HTML code update to link to a stylesheet
+- add the new static resources to the configuration
+http://localhost:8080/
 """
 
 
@@ -16,7 +21,9 @@ class StringGenerator(object):
     @cherrypy.expose
     def index(self):
         return """<html>
-          <head></head>
+          <head>
+          <link href="/static/css/style.css" rel="stylesheet">
+          </head>
           <body>
             <form method="get" action="generate">
               <input type="text" value="8" name="length" />
@@ -40,6 +47,11 @@ if __name__ == '__main__':
     conf = {
         '/': {
             'tools.sessions.on': True
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+          'tools.staticdir.on': True,
+          'tools.staticdir.dir': './public'
         }
     }
     cherrypy.quickstart(StringGenerator(), '/', conf)
