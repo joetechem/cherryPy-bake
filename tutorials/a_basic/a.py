28cec31470b571463fb34cab4c94d3b83961c910
joetechem <joe@techemstudios.com>
2019-02-14 12:22:11 -0500

    many urls are better than 1

--- tutorials/a_basic/a.py
+++ tutorials/a_basic/a.py
@@ -1,11 +1,13 @@
-import cherrypy
+"""
+http://localhost:8080
+"""
 
+import cherrypy
 
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
         return "Hello world!"
-
 
 if __name__ == '__main__':
     cherrypy.quickstart(HelloWorld())
---
+++ tutorials/a_basic/b.py
@@ -0,0 +1,22 @@
+"""
+http://localhost:8080/generate
+"""
+
+import random
+import string
+
+import cherrypy
+
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+    @cherrypy.expose
+    def generate(self):
+        return ''.join(random.sample(string.hexdigits, 8))
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(StringGenerator())
